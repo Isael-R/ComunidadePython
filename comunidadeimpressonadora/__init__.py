2b7509a7afa44efe0d578e431fabc4d096674cd0
Isael-R <129337795+Isael-R@users.noreply.github.com>
2023-03-29 19:38:02 -0300

    Update __init__.py

--- comunidadeimpressonadora/__init__.py
+++ comunidadeimpressonadora/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = '59899a3b063a041501206ddd6a7ab2f4'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
